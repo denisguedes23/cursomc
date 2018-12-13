392e5e458cc2c79a2b05de5ca6973afaaf9e96d0
Denis Guedes <denisguedes23@gmail.com>
2018-12-13 15:13:06 -0300

    Implementação tratamento de erro

--- src/main/java/com/curso/services/CategoriaService.java
+++ src/main/java/com/curso/services/CategoriaService.java
@@ -6,14 +6,20 @@ import org.springframework.stereotype.Service;
 import com.curso.domain.Categoria;
 import com.curso.repositories.CategoriaRepository;
 
+import javassist.tools.rmi.ObjectNotFoundException;
+
 @Service
 public class CategoriaService {
 	
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
+	public Categoria buscar(Integer id) throws ObjectNotFoundException {
 		Categoria categoria = repo.findOne(id);
+		if(categoria == null) {
+				throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id 
+						+ ", Tipo: " + Categoria.class.getName());
+		}
 		return categoria;
 	}
 }
