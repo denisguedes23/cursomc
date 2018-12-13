e8bb74dbb4dcd34994fc75ecfbd942e9e6589a96
Denis Guedes <denisguedes23@gmail.com>
2018-12-13 17:00:42 -0300

    Cliente Service.

---
+++ src/main/java/com/curso/services/ClienteService.java
@@ -0,0 +1,25 @@
+package com.curso.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.curso.domain.Categoria;
+import com.curso.repositories.CategoriaRepository;
+
+import javassist.tools.rmi.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) throws ObjectNotFoundException {
+		Categoria categoria = repo.findOne(id);
+		if(categoria == null) {
+				throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id 
+						+ ", Tipo: " + Categoria.class.getName());
+		}
+		return categoria;
+	}
+}
