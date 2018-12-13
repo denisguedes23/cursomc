480fb28070d35ca9593be88a17495380ccfb35bf
Denis Guedes <denisguedes23@gmail.com>
2018-12-13 10:53:41 -0300

    Configuracao banco de dados H2

--- src/main/java/com/curso/resources/CategoriaResource.java
+++ src/main/java/com/curso/resources/CategoriaResource.java
@@ -1,15 +1,26 @@
 package com.curso.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.curso.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!";
+	public List<Categoria> listar() {
+		Categoria categoria = new Categoria(1, "Informatica");
+		Categoria categoria2 = new Categoria(2, "Informatica");
+		
+		List<Categoria> categorias = new ArrayList<Categoria>();
+		categorias.add(categoria);
+		categorias.add(categoria2);
+		return categorias;
 	}
 }
